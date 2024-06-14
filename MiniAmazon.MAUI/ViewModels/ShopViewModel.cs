ba568a36d49e704ac5495354daa304aba8de8096
ryan_ly <ryanly_PC>
2024-06-14 18:15:20 -0400

    Improved UI for ShopView, Created Search Bar Functionality, Created Split View for Inventory and Cart

---
+++ MiniAmazon.MAUI/ViewModels/ShopViewModel.cs
@@ -0,0 +1,57 @@
+﻿using MiniAmazon.Library.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniAmazon.MAUI.ViewModels
+{
+    public class ShopViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void RefreshInventory()
+        {
+            SearchInventoryQuery = string.Empty;
+            NotifyPropertyChanged(nameof(Products));
+        }
+
+        public void SearchInventory()
+        {
+            NotifyPropertyChanged(nameof(Products));
+        }
+        public ShopViewModel()
+        {
+            SearchInventoryQuery = string.Empty;
+        }
+
+        private string searchInventoryQuery;
+
+        public string SearchInventoryQuery
+        {
+            set
+            {
+                searchInventoryQuery = value;
+                NotifyPropertyChanged();
+            }
+            get { return searchInventoryQuery; }
+        }
+        public List<ProductViewModel> Products
+        {
+            get
+            {
+                return InventoryService.Current?.Products?.Where(p => p != null)
+                    .Where(p => p.Name?.ToUpper()?.Contains(SearchInventoryQuery.ToUpper()) ?? false)
+                    .Select(p => new ProductViewModel(p)).ToList() ?? new List<ProductViewModel>();
+            }
+        }
+    }
+}
--- MiniAmazon.MAUI/Views/ShopView.xaml.cs
+++ MiniAmazon.MAUI/Views/ShopView.xaml.cs
@@ -1,3 +1,5 @@
+using MiniAmazon.MAUI.ViewModels;
+
 namespace MiniAmazon.MAUI.Views;
 
 public partial class ShopView : ContentPage
@@ -5,10 +7,26 @@ public partial class ShopView : ContentPage
 	public ShopView()
 	{
 		InitializeComponent();
+		BindingContext = new ShopViewModel();
 	}
 
     private void Logo_Clicked(object sender, EventArgs e)
     {
 		Shell.Current.GoToAsync("//MainPage");
+    }
+
+    private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
+    {
+        (BindingContext as ShopViewModel)?.RefreshInventory();
+    }
+
+    private void SearchInventory_Clicked(object sender, EventArgs e)
+    {
+        (BindingContext as ShopViewModel)?.SearchInventory();
+    }
+
+    private void RefreshSearch_Clicked(object sender, EventArgs e)
+    {
+        (BindingContext as ShopViewModel)?.RefreshInventory();
     }
 }
